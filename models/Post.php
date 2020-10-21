4309036177d3d301bacfadc9665b258b0c8f3293
alexandrov-vasiliy <zowel.vasili@yandex.ru>
2020-10-21 22:51:50 +0300

    lab2

---
+++ models/Post.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models;
+use yii\db\Expression;
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $created_at
+ * @property string $updated_at
+ * @property int $author_id
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string'],
+            [['created_at', 'update_time'], 'safe'],
+            [['author_id'], 'integer'],
+            [['updater_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'author_id' => 'Author ID',
+            'updater_id' => 'Updater ID',
+        ];
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => 'updated_at',
+                'value' => new Expression('NOW()'),
+            ],
+            [
+                'class' => BlameableBehavior::className(),
+                'createdByAttribute' => 'author_id',
+                'updatedByAttribute' => 'updater_id',
+            ],
+        ];
+    }
+}
